229847b4e691424155520d3512125a92dd0adbaf
Higor <higor.xsapo@gmail.com>
2015-06-01 12:32:34 -0300

    Diminuindo intervalo de troca de banner

--- principal.js
+++ principal.js
@@ -1,4 +1,4 @@
-var = banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
 
 function trocaBanner() {
@@ -6,4 +6,4 @@ function trocaBanner() {
 	document.querySelector('h2#mensagem').texteContent = banners[bannerAtual];
 }
 
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1000);
